d524fcc0e751b5db1d59d09f1e1054c5a33f49dd
Edison <edisonkpaul61@gmail.com>
2026-05-28 18:33:08 +0530

    Add api test route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/api-test', function () {
+    return response()->json([
+        'success' => true,
+        'message' => 'API test from web route working',
+    ]);
+});
